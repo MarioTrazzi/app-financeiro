bf08e828edc08c29e9005a150b793961c4618414
MarioTrazzi <mariotrazzi@gmail.com>
2023-04-27 15:50:00 -0300

    Add files via upload

---
+++ Row2.tsx
@@ -0,0 +1,50 @@
+import DashboardBox from "@/components/DashboardBox";
+import React, { useState } from 'react'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+//const containerStyle = {
+//  width: '100%',
+//  height: '100%'
+//};
+
+const center = {
+  lat: -20.418736, 
+  lng: -49.975680
+};
+
+
+const Row2 = () => {
+ 
+  const [marker, setMarker] = useState({
+    position: center,
+    icon: 'https://icons.veryicon.com/png/o/miscellaneous/display-icon-on-internet-of-things-map/optical-box-3.png'
+     
+  });
+
+  
+  return (
+   <DashboardBox gridArea = "b">
+    <LoadScript
+      googleMapsApiKey={'AIzaSyDQmihtWJICIdIBgqZ_bhFhpW2pgQI9lJs'}
+    >
+      <GoogleMap
+        mapContainerStyle={{width: '100%', height: '100%'}}
+        center={center}
+        zoom={15}
+      >
+        <Marker
+             label='106'
+             position={marker.position}
+             icon={{
+             url: marker.icon,
+             scaledSize: 60
+                  
+          }}
+        />
+      </GoogleMap>
+    </LoadScript>
+    </DashboardBox>
+    
+  );
+        };
+ export default Row2;
